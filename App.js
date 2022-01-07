c883becc6bd8a057ccd8f8f55a718260ae993be9
aref-mehran <aref.mehran.w@gmail.com>
2022-01-07 21:00:31 +0330

    react-native-paper added and works

--- App.js
+++ App.js
@@ -1,31 +1,37 @@
-import * as React from 'react';
-import { Text, View, StyleSheet, Button } from 'react-native';
-import { Audio } from 'expo-av';
-
+import * as React from "react";
+import { Text, View, StyleSheet, Button } from "react-native";
+import { Audio } from "expo-av";
+import ButtonExample from "./ButtonExample";
 export default function App() {
   const [sound, setSound] = React.useState();
 
   async function playSound() {
-    console.log('Loading Sound');
+    console.log("Loading Sound");
     const { sound } = await Audio.Sound.createAsync(
-       require('./assets/Hello.mp3')
+      require("./assets/Hello.mp3")
     );
     setSound(sound);
 
-    console.log('Playing Sound');
-    await sound.playAsync(); }
+    console.log("Playing Sound");
+    await sound.playAsync();
+  }
 
   React.useEffect(() => {
     return sound
       ? () => {
-          console.log('Unloading Sound');
-          sound.unloadAsync(); }
+          console.log("Unloading Sound");
+          sound.unloadAsync();
+        }
       : undefined;
   }, [sound]);
 
   return (
     <View style={styles.container}>
       <Button title="Play Sound" onPress={playSound} />
+      <Button title="Play Sound" onPress={playSound} />
+      <Button title="Play Sound" onPress={playSound} />
+      <Button title="Play Sound" onPress={playSound} />
+      <ButtonExample />
     </View>
   );
 }
@@ -33,8 +39,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    backgroundColor: '#ecf0f1',
-    padding: 10,
-  },
+    justifyContent: "center",
+    backgroundColor: "#ecf0f1",
+    padding: 10
+  }
 });
